0e48f2ed9902cac684337bea6ce6e81850b416a9
Aakash Soni <inos92@gmail.com>
2023-02-28 20:55:15 -0600

    added route to filter products under price 20

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,9 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products.as_json
   end
+
+  def products_under_20
+    products = Product.where("price < '20'")
+    render json: products.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "all_products"
+
+  get "/products_under_20", controller: "products", action: "products_under_20"
 end
