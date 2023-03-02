8e74d79345d6dc3eb9e5323740f68755486aa264
Aakash Soni <inos92@gmail.com>
2023-03-01 19:41:41 -0600

    created tests for mini-capstone-api, updated route to RESTful index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get "/all_products", controller: "products", action: "all_products"
+  # get "/all_products", controller: "products", action: "all_products"
 
-  get "/products_under_20", controller: "products", action: "products_under_20"
+  # get "/products_under_20", controller: "products", action: "products_under_20"
+
+  get "/all_products" => "products#all_products"
+
+  get "/products_under_20" => "products#products_under_20"
 end
