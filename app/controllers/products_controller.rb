6e29d70ce93502fca88647844adb8454165f30dc
Aakash Soni <inos92@gmail.com>
2023-03-02 21:34:18 -0600

    added views to customize json web response

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,10 @@ class ProductsController < ApplicationController
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
 
   def index
-    products = Product.all
-    render json: products.as_json
+    # products = Product.all
+    # render json: products.as_json
+    @products = Product.all
+    render template: "products/index"
   end
 
   def products_under_20
@@ -13,8 +15,10 @@ class ProductsController < ApplicationController
 
   def show
     product_id = params[:id]
-    product = Product.find_by(id: product_id)
-    render json: product.as_json
+    # product = Product.find_by(id: product_id)
+    # render json: product.as_json
+    @product = Product.find_by(id: product_id)
+    render template: "products/show"
   end
 
   def create
