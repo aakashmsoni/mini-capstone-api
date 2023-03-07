dddb815f0618c1a819b6a3445fc2806b67649e05
Aakash Soni <inos92@gmail.com>
2023-03-06 18:55:53 -0600

    added validation for name, price, description

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   validates :name, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :description, length: { in: 10..500 }
 end
