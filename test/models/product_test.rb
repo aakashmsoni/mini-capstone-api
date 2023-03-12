398fbcecdf8e766fa9d695098268c24ba031ffa0
Aakash Soni <inos92@gmail.com>
2023-03-12 16:52:03 -0500

    updated test models and HTTP req

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -8,4 +8,14 @@ class ProductTest < ActiveSupport::TestCase
     product = Product.new(price: 1)
     assert_equal true, product.is_discounted?
   end
+
+  test "tax" do
+    product = Product.new(price: 100)
+    assert_in_delta 9, product.tax
+  end
+
+  test "total" do
+    product = Product.new(price: 100)
+    assert_in_delta 109, product.total
+  end
 end
