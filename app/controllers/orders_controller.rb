7aca791d0be664a400417187fc5e597fb170d569
Aakash Soni <inos92@gmail.com>
2023-03-14 19:48:51 -0500

    created partial in views for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   def index
-    orders = Order.all
+    orders = current_user.orders
     render json: orders.as_json
   end
 
@@ -11,13 +11,16 @@ class OrdersController < ApplicationController
 
   def create
     product = Product.find_by(id: params[:product_id])
+    subtotal = (params[:quantity] * product.price)
+    tax = (0.09 * (product.price * params[:quantity]))
+    total = subtotal + tax
     order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: (product.price * params[:quantity]),
-      tax: (0.09 * (product.price * params[:quantity])),
-      total: ((0.09 * (product.price * params[:quantity])) + (product.price * params[:quantity])),
+      subtotal: subtotal,
+      tax: tax,
+      total: total,
     )
     if order.save
       order.save
