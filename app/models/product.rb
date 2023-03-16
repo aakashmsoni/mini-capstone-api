e2c322bed1b5f1689fa50b2536b85a844ec44af0
Aakash Soni <inos92@gmail.com>
2023-03-15 20:54:43 -0500

    added has_many/belongs_to relationships to product, category, category_products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,13 +3,14 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, length: { in: 0..500 }, presence: true
   has_many :orders
+  has_many :category_products
+  has_many :images
   belongs_to :supplier
   # product belongs_to :supplier, therefore supplier_id on products table
   # def supplier
   #   Supplier.find_by(id: supplier_id)
   # end
 
-  has_many :images
   # product has_many :images, therefore product_id on images table
   # def images
   #   Image.where(product_id: id)
