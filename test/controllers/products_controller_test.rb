97d30249fa7a2fcefdce9b449121a736753fec78
Aakash Soni <inos92@gmail.com>
2023-03-15 19:45:56 -0500

    updated create, update, destroy in product_controller_tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,6 +1,14 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create(name: "Test", email: "test@test.com", password: "password", admin: true)
+    @order = Order.create(user_id: @user.id, product_id: Product.first.id, quantity: 10)
+    post "/sessions.json", params: { email: "test@test.com", password: "password" }
+    data = JSON.parse(response.body)
+    @jwt = data["jwt"]
+  end
+
   test "index" do
     get "/products.json"
     assert_response 200
@@ -26,29 +34,36 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
           inventory: 1,
           description: "test description",
           supplier_id: Supplier.first.id,
-        }
+        },
+        headers: { "Authorization" => "Bearer #{@jwt}" }
       assert_response 200
     end
 
-    post "/products.json", params: {}
+    post "/products.json", params: {}, headers: { "Authorization" => "Bearer #{@jwt}" }
     assert_response 422
+
+    post "/products.json", params: {}
+    assert_response 401
   end
 
   test "update" do
     product = Product.first
-    patch "/products/#{product.id}.json", params: { name: "Crystal Skull" }
+    patch "/products/#{product.id}.json", params: { name: "Crystal Skull" }, headers: { "Authorization" => "Bearer #{@jwt}" }
     assert_response 200
 
     data = JSON.parse(response.body)
     assert_equal "Crystal Skull", data["name"]
 
-    patch "/products/#{product.id}.json", params: { name: "" }
+    patch "/products/#{product.id}.json", params: { name: "" }, headers: { "Authorization" => "Bearer #{@jwt}" }
     assert_response 422
+
+    patch "/products/#{product.id}.json", params: { name: "" }
+    assert_response 401
   end
 
   test "destroy" do
     assert_difference "Product.count", -1 do
-      delete "/products/#{Product.first.id}.json"
+      delete "/products/#{Product.first.id}.json", headers: { "Authorization" => "Bearer #{@jwt}" }
       assert_response 200
     end
   end
